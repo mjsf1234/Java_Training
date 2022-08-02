d9aa858183fa529044abaafe87f4e20ad15d3be7
Mrityunjay saraf <mrityunjaysaraf5678@gmail.com>
2022-08-03 01:00:03 +0530

    addded getter and setters

--- quidditch-game/src/main/Main.java
+++ quidditch-game/src/main/Main.java
@@ -5,55 +5,60 @@ import src.main.models.Team;
 
 public class Main {
 
-    static Game game;
+        static Game game;
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        System.out.println(new Team("", "Oliver", "Harry",
-                new String[] { "Angelina", "Ginny", "Katie" }));
+                Team home = new Team("GRYFFINDOR", "Oliver", "Harry",
+                                new String[] { "Angelina", "Ginny", "Katie" });
 
-        System.out.println(new Team("SLYTHERIN", "Vincent", "Draco",
-                new String[] { "Bridget", "Harper", "Malcolm" }));
+                Team away = new Team("SLYTHERIN", "Vincent", "Draco",
+                                new String[] { "Bridget", "Harper", "Malcolm" });
 
-    }
+                Game game = new Game(home, away);
 
-    /**
-     * Function name: getData`
-     * 
-     * @return (String[][])
-     * @throws FileNotFoundException
-     * 
-     *                               Inside the function:
-     *                               1. Returns data from TEAMS_FILE as a String[][]
-     *                               array
-     */
+                game.getScore(home);
+                game.setScore(home, 10);
 
-    /**
-     * Function name: startGame
-     * 
-     * Inside the function:
-     * 1. Grabs each play from plays.txt and calls game.simulate(play);
-     * 2. Prints the return from game.simulate(play)
-     * - println("\n" + <return> + "\n");
-     */
+        }
 
-    /**
-     * Function name: printResult()
-     * 
-     * Inside the function:
-     * 1. Prints the final score: println("\nGRYFFINDOR: " + <gryffindor score> + "
-     * SLYTHERIN: " + <slytherin score>);
-     * 2. Prints the winner: println("\n" + <winner team name> + " WINS!");
-     * 
-     */
+        /**
+         * Function name: getData`
+         * 
+         * @return (String[][])
+         * @throws FileNotFoundException
+         * 
+         *                               Inside the function:
+         *                               1. Returns data from TEAMS_FILE as a String[][]
+         *                               array
+         */
 
-    /**
-     * Function name: wait
-     * 
-     * @param sec
-     * 
-     *            Inside the function:
-     *            1. Make the code sleep for X seconds.
-     */
+        /**
+         * Function name: startGame
+         * 
+         * Inside the function:
+         * 1. Grabs each play from plays.txt and calls game.simulate(play);
+         * 2. Prints the return from game.simulate(play)
+         * - println("\n" + <return> + "\n");
+         */
+
+        /**
+         * Function name: printResult()
+         * 
+         * Inside the function:
+         * 1. Prints the final score: println("\nGRYFFINDOR: " + <gryffindor score> + "
+         * SLYTHERIN: " + <slytherin score>);
+         * 2. Prints the winner: println("\n" + <winner team name> + " WINS!");
+         * 
+         */
+
+        /**
+         * Function name: wait
+         * 
+         * @param sec
+         * 
+         *            Inside the function:
+         *            1. Make the code sleep for X seconds.
+         */
 
 }
--- quidditch-game/src/main/models/Game.java
+++ quidditch-game/src/main/models/Game.java
@@ -1,7 +1,25 @@
 package src.main.models;
 
+import java.util.HashMap;
+
 public class Game {
 
+    private HashMap<Team, Integer> scoreBoard;
 
+    public Game(Team home, Team away) {
+        this.scoreBoard = new HashMap<Team, Integer>();
+
+        scoreBoard.put(new Team(home), 0);
+        scoreBoard.put(new Team(away), 0);
+
+    }
+
+    public Integer getScore(Team team) {
+        return scoreBoard.get(team);
+    }
+
+    public void setScore(Team team, Integer score) {
+        scoreBoard.put(team, score);
+    }
 
 }
